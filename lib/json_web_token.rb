65f10e1a3665506b832528617065362df185d5f5
Constantine Vetoshev <constantine@precise.ly>
2020-03-30 18:06:40 -0700

    Change configuration settings.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -13,7 +13,7 @@ class JsonWebToken
                algorithm: 'RS256',
                # XXX: There MUST be a trailing slash at the end of the iss
                # parameter, or validation fails!
-               iss: Utils.ensure_trailing_slash(Rails.configuration.auth0.url),
+               iss: Utils.ensure_trailing_slash("https://#{Rails.configuration.auth0.domain}/"),
                verify_iss: true,
                aud: Rails.configuration.auth0.audience,
                verify_aud: true) do |header|
@@ -22,7 +22,7 @@ class JsonWebToken
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("#{Rails.configuration.auth0.url}/.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("https://#{Rails.configuration.auth0.domain}/.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys.map do |k|
