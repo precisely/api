29c761b92b65849a8c102766aeb0daa2030311be
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-04-27 04:51:21 -0700

    Address minitest parallelization bug
    See:
     https://github.com/rails/rails/issues/32682
     https://github.com/rails/rails/issues/38424

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,14 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # include Devise::Test::IntegrationHelpers
 
+  # Note: the following causes an error in Minitest 5.14
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors) 
+  # See: https://github.com/rails/rails/issues/32682
+  # and https://github.com/rails/rails/issues/38424
+  
+  # instead, limit to 1 worker:
+  parallelize(workers: 1)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
