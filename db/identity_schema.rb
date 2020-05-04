20b4d04b70fdd33c615014725703be007d27db2d
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-05-04 13:32:51 -0700

    Add jwt-blacklist table

--- db/identity_schema.rb
+++ db/identity_schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_003606) do
+ActiveRecord::Schema.define(version: 2020_04_27_150559) do
+
+  create_table "jwt_blacklist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_blacklist_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
