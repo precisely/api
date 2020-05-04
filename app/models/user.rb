402c67308241e5ab16e62204b195d24c1151d245
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-05-04 13:52:10 -0700

    Update various files to get devise-jwt working

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,21 @@ class User < IdentityRecord
   # :confirmable, :lockable, :invitable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :lockable, :confirmable, :timeoutable, :trackable
+         :lockable, :confirmable, :timeoutable, :trackable,
+         :jwt_authenticatable, :registerable,
+         jwt_revocation_strategy: JwtBlacklist
          
   validates_uniqueness_of :email, :case_sensitive => false
+
+  # Inject data into the JWT Token
+  def jwt_payload 
+    { email: email } # user_id already provided as 'sub' key
+  end
+
+  # Executed after a token is generated for a User
+  def on_jwt_dispatch(token, payload)
+    # do_something(token, payload)
+  end
+
 end
 
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,2 @@
+# disable sessions - according to https://github.com/waiting-for-dev/devise-jwt#session-storage-caveat
+Rails.application.config.session_store :disabled
