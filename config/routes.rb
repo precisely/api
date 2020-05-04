f4ea650a83fb54bcef81ab10abfa6e4e8f4e12ad
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-05-04 13:33:10 -0700

    Remove users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :user
-  resources :users
   root to: "home#index"
-  post 'login', controller: 'login'
-  post 'logout', controller: 'login'
-  post 'signup', controller: 'login'
-  post 'reset_password', controller: 'login'
+  
+  devise_for :user,
+    path: 'auth',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup' 
+    }, 
+    controllers: {
+      registrations: 'registrations'
+    }
 
-  get 'public', controller: 'public'
-  get 'private', controller: 'private'
-  get 'lab', controller: 'private'
-  get 'random', controller: 'private'
-  get 'admin', controller: 'private'
 end
