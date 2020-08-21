c104dc175ed702d1664fe89ef197643b40c79045
Constantine Vetoshev <constantine@precise.ly>
2020-08-21 00:03:54 -0700

    Once more, with feeling.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
     address: "smtp.sendgrid.net",
     port: 587,
     domain: ENV["EMAIL_DOMAIN"],
-    user_name: Rails.application.credentials.dig(:sendgrid, :key, ENV["STAGE"].to_sym),
+    user_name: "apikey",
     password: Rails.application.credentials.dig(:sendgrid, :key, ENV["STAGE"].to_sym),
     authentication: 'plain',
     enable_starttls_auto: true
